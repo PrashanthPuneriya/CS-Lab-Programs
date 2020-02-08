87af3d0832576a1249b8902c8fd07c16e09e72e8
Prashanth Puneriya <puneriyaprashanth@gmail.com>
2020-02-08 11:53:07 +0530

    Added comments for MergeSort.c

--- Data Structures/Sorting/MergeSort.c
+++ Data Structures/Sorting/MergeSort.c
@@ -1,35 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void Merge (int arr[], int left, int mid, int right) {
+void Merge(int arr[], int left, int mid, int right) {
     int n1 = mid - left + 1;   //index is counted from 0. Hence, mid + 1
     int n2 = right - mid;
     int i, j, k;
     int L[n1], R[n2];
 
-    for (i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (j = 0; j < n2; j++) 
-        R[j] = arr[mid + 1 + j];
+    for(i = 0; i < n1; i++)
+        L[i] = arr[left + i];   // sorted L array
+    for(j = 0; j < n2; j++) 
+        R[j] = arr[mid + 1 + j];    // sorted R array
 
     i = 0;
     j = 0;
     k = left;
 
-    while (i < n1 || j < n2) {
-        if (i < n1 && L[i] <= R[j]) {
-            arr[k] = L[i]; 
+    while(i < n1 || j < n2) {
+        if(i < n1 && L[i] <= R[j]) {   // compare L and R array elements(sorted) and then push it into arr array. This makes arr sorted.
+            arr[k] = L[i]; // Push the L array elements(sorted) until its get empty
             i++;
         }
         else {
-            arr[k] = R[j]; 
+            arr[k] = R[j];  // Push the remaining R array elements(sorted) into the arr array
             j++;
         }
         k++;
     }
 }
 
-void MergeSort (int arr[], int left, int right) {
+void MergeSort(int arr[], int left, int right) {
     if (left < right) {
         int mid = (left + right) / 2;   // defines the current array in 2 parts .
         MergeSort (arr, left, mid);   // sort the 1st part of array .
@@ -46,8 +46,7 @@ void printArray(int A[], int size)
         printf("%d ", A[i]); 
     printf("\n"); 
 } 
-  
-/* Driver program to test above functions */
+
 int main() 
 { 
     int arr[] = {12, 11, 13, 5, 6, 7}; 
